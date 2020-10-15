801d7af239d57833a75fd88f0b7ab9d0c6e54987
sdpereira <sdpereira@misena.edu.co>
2020-10-14 19:47:28 -0500

    Sidebar

--- resources/views/plantilla/sidebar.blade.php
+++ resources/views/plantilla/sidebar.blade.php
@@ -5,29 +5,40 @@
                 <a class="nav-link active" href="#"><i class="icon-speedometer"></i> Escritorio</a>
             </li>
             <li class="nav-title">
-                Maestras
+                MENÚ
             </li>
             <li class="nav-item nav-dropdown">
                 <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-bag"></i> General</a>
                 <ul class="nav-dropdown-items">
                     <li @click="menu=1" class="nav-item">
-                        <a class="nav-link" href="#"><i class="icon-bag"></i> Componente 1</a>
+                        <a class="nav-link" href="#"><i class="fa fa-user-circle-o"></i> Personas</a>
                     </li>
                     <li @click="menu=2" class="nav-item">
-                        <a class="nav-link" href="#"><i class="icon-bag"></i>Personas</a>
+                        <a class="nav-link" href="#"><i class="fa fa-shopping-bag"></i>Productos</a>
                     </li>
                     <li @click="menu=3" class="nav-item">
-                        <a class="nav-link" href="#"><i class="icon-bag"></i> Componente 3</a>
+                        <a class="nav-link" href="#"><i class="fa fa-university"></i> Sucursales</a>
                     </li>
                     <li @click="menu=4" class="nav-item">
-                        <a class="nav-link" href="#"><i class="icon-bag"></i>Componente 4</a>
+                        <a class="nav-link" href="#"><i class="fa fa-money"></i>Ventas</a>
                     </li>
                     <li @click="menu=5" class="nav-item">
-                        <a class="nav-link" href="#"><i class="icon-bag"></i> Roles</a>
+                        <a class="nav-link" href="#"><i class="fa fa-server"></i> Roles</a>
+                    </li>
+                    <li @click="menu=6" class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-group"></i> Usuarios</a>
+                    </li>
+                    <li @click="menu=8" class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-cubes"></i> Bodegas</a>
+                    </li>
+                    <li @click="menu=9" class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-building"></i> Empresas</a>
                     </li>
                 </ul>
             </li>
+            
         </ul>
     </nav>
     <button class="sidebar-minimizer brand-minimizer" type="button"></button>
 </div>
+
